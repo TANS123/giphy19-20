db7c633fd93c5a5d916698d3bb15b93c5e1c688f
TANS123 <s_tiera.secrease@ousd.org>
2020-03-05 02:10:56 +0000

    css and random

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
 /* global $ */
 
 console.log('hi');
-
-$('h1').click(function(){
+$(".back").hide();
+$('.search').click(function(){
     $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+    $('h1').text('Awesomeness');
 });
-fetch("https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
+$('.search').click(function(){ 
+var searchTerm = $('input').val();
+var result = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY";
+fetch(result)
 .then(function(response) {
 return response.json();
 })
 .then(function(data) {
-  console.log(data.data[0]);
+  console.log(data.data.length);
+  var rand = Math.floor(Math.random() * 25);
+  var gif_url = data.data[rand].images.original.url;
+  console.log(gif_url);
+  $('body').append('<img src=' + gif_url + '>');
 })
+$(".search").hide();
+$("input").hide();
+$(".back").show();
+});
+$('.back').click(function(){
+  $(".back").hide();
+  $(".search").show();
+  $("input").show();
+  $("img").hide();
+  $('h1').text('Giphy Project');
+});
+
+
+// .then(function(data) {
+//   console.log(data.data[0]);
+// })
